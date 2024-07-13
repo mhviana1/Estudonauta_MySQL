6c9aaebf0da3d26a97a92a17c0d0b2d5659b3404
mhviana1 <mhviana.adm@gmail.com>
2024-07-13 14:01:35 -0300

    terceuri envio

--- 002.sql
+++ 002.sql
@@ -8,4 +8,10 @@ ano year default '2016'
 ) default charset=utf8;
 desc cursos;
 
+alter table cursos
+add column idcurso int first;
 
+alter table cursos
+add primary key (idcurso);
+
+desc cursos;
--- Untitled.sql
+++ Untitled.sql
@@ -45,11 +45,60 @@ modify column profissao varchar(30);
 
 alter table pessoas
 rename to garfanhotos;
+desc garfanhotos;
+select * from garfanhotos;
+select * from cursos;
+desc cursos;
 
+insert into cursos values
+('1', 'Matemática Financeira', 'Curso Inicial de Matemática', '40', '15', '2024');
+select * from cursos;
+INSERT INTO cursos (nome, descricao, carga, totaulas, ano) VALUES
+('Matemática Básica', 'Curso de introdução à matemática básica.', 40, 20, 2016),
+('História do Brasil', 'Estudo dos principais eventos históricos do Brasil.', 60, 30, 2017),
+('Programação em Python', 'Curso completo de programação em Python.', 80, 40, 2018),
+('Inglês para Iniciantes', 'Curso básico de inglês.', 50, 25, 2016),
+('Física Fundamental', 'Curso introdutório de física.', 70, 35, 2019),
+('Química Orgânica', 'Estudo da química dos compostos de carbono.', 90, 45, 2020),
+('Biologia Celular', 'Curso sobre a estrutura e função das células.', 60, 30, 2016),
+('Geografia Geral', 'Estudo dos principais aspectos geográficos do mundo.', 55, 27, 2021),
+('Literatura Brasileira', 'Análise das principais obras da literatura brasileira.', 45, 22, 2016),
+('Introdução à Filosofia', 'Curso introdutório aos principais conceitos filosóficos.', 65, 32, 2016);
 
+INSERT INTO cursos (idcurso, nome, descricao, carga, totaulas, ano) VALUES
+(2, 'História do Brasil', 'Estudo dos principais eventos históricos do Brasil.', 60, 30, 2017),
+(3, 'Programação em Python', 'Curso completo de programação em Python.', 80, 40, 2018),
+(4, 'Inglês para Iniciantes', 'Curso básico de inglês.', 50, 25, 2016),
+(5, 'Física Fundamental', 'Curso introdutório de física.', 70, 35, 2019),
+(6, 'Química Orgânica', 'Estudo da química dos compostos de carbono.', 90, 45, 2020),
+(7, 'Biologia Celular', 'Curso sobre a estrutura e função das células.', 60, 30, 2016),
+(8, 'Geografia Geral', 'Estudo dos principais aspectos geográficos do mundo.', 55, 27, 2021),
+(9, 'Literatura Brasileira', 'Análise das principais obras da literatura brasileira.', 45, 22, 2016),
+(10, 'Introdução à Filosofia', 'Curso introdutório aos principais conceitos filosóficos.', 65, 32, 2016);
+select * from cursos;
 
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
 
+select * from cursos;
 
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
 
+update cursos
+set nome = 'Java', carga = '10', ano = '2010'
+where idcurso = '5'
+limit 1;
 
+update cursos
+set ano = '2099'
+where ano = '2016';
 
+select * from cursos;
+
+delete from cursos
+where idcurso = '8';
+
+select * from cursos;
